d9fe169f416d2758e75a76aec886db24182c4cbd
Scott Van Gilder <svangilder@gmail.com>
2016-08-28 23:04:48 -0600

    bug fix game.js

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -23,87 +23,87 @@ drMarioGame.Game.prototype = {
     // TODO: FIGURE OUT WHERE THE CODE IS BREAKING!!!
     // TODO: CODE BREAKS BELOW THIS LINE
 
-    // this.createVirus();
-    // this.createPills();
+    this.createVirus();
+    this.createPills();
 
     //create pill
     // TODO: have the game create a random pill color combo
-    // var result = this.findObjectsByType('playerStart', this.map, 'objectsLayer')
-    // this.player = this.game.add.sprite(result[0].x, result[0].y, 'playerStart');
-    // this.game.physics.arcade.enable(this.player);
+    var result = this.findObjectsByType('playerStart', this.map, 'objectsLayer')
+    this.player = this.game.add.sprite(result[0].x, result[0].y, 'playerStart');
+    this.game.physics.arcade.enable(this.player);
 
 
     // move player with cursor keys
-  //   this.cursors = this.game.input.keyboard.createCursorKeys();
-  //
-  // },
+    this.cursors = this.game.input.keyboard.createCursorKeys();
 
-  // createVirus: function() {
-    //create virus's
-    // this.items = this.game.add.group();
-    // this.items.enableBody = true;
-    // var item;
-    // result = this.findObjectsByType('enemy', this.map, 'objectsLayer');
-    // result.forEach(function(element) {
-      // this.createFromTiledObject(element, this.enemy);
-    // }, this);
-  // },
-  // createPills: function() {
-    //create doors
-    // this.doors = this.game.add.group();
-    // this.doors.enableBody = true;
-    // result = this.findObjectsByType('pill', this.map, 'objectsLayer');
+  },
 
-    // result.forEach(function(element) {
-      // this.createFromTiledObject(element, this.doors);
-    // }, this);
-  // },
+  createVirus: function() {
+  //   create virus's
+    this.virus = this.game.add.group();
+    this.virus.enableBody = true;
+    var item;
+    result = this.findObjectsByType('virus', this.map, 'objectsLayer');
+    result.forEach(function(element) {
+      this.createFromTiledObject(element, this.virus);
+    }, this);
+  },
+  createPills: function() {
+    // create pills
+    this.pills = this.game.add.group();
+    this.pills.enableBody = true;
+    result = this.findObjectsByType('pill', this.map, 'objectsLayer');
+
+    result.forEach(function(element) {
+      this.createFromTiledObject(element, this.pills);
+    }, this);
+  },
 
   // find objects in a Tiled layer that containt a property called "type" equal to a certain value
-  // findObjectsByType: function(type, map, layer) {
-    // var result = new Array();
-    // map.objects[layer].forEach(function(element) {
-      // if (element.properties.type === type) {
-        //Phaser uses top left, Tiled bottom left so we have to adjust
-        //also keep in mind that the cup images are a bit smaller than the tile which is 16x16
-        //so they might not be placed in the exact position as in Tiled
-        // element.y -= map.tileHeight;
-        // result.push(element);
-      // }
-    // });
-    // return result;
-  // },
+  findObjectsByType: function(type, map, layer) {
+    var result = new Array();
+    map.objects[layer].forEach(function(element) {
+      if (element.properties.type === type) {
+        // Phaser uses top left, Tiled bottom left so we have to adjust
+        // also keep in mind that the cup images are a bit smaller than the tile which is 16x16
+        // so they might not be placed in the exact position as in Tiled
+        element.y -= map.tileHeight;
+        result.push(element);
+      }
+    });
+    return result;
+  },
   //create a sprite from an object
-  // createFromTiledObject: function(element, group) {
-    // var sprite = group.create(element.x, element.y, element.properties.sprite);
+  createFromTiledObject: function(element, group) {
+    var sprite = group.create(element.x, element.y, element.properties.sprite);
 
     //copy all properties to the sprite
-    // Object.keys(element.properties).forEach(function(key) {
-      // sprite[key] = element.properties[key];
-    // });
-  // },
+    Object.keys(element.properties).forEach(function(key) {
+      sprite[key] = element.properties[key];
+    });
+  },
 
-  // update: function() {
+  update: function() {
     //collision
-    // this.game.physics.arcade.collide(this.player, this.blockedLayer);
-    // this.game.physics.arcade.overlap(this.player, this.items, this.collect, null, this);
+    this.game.physics.arcade.collide(this.player, this.blockedLayer);
+    this.game.physics.arcade.overlap(this.player, this.items, this.collect, null, this);
 
     //player movement
-    // this.player.body.velocity.x = 0;
+    this.player.body.velocity.x = 0;
 
-    // if (this.cursors.up.isDown) {
-      // if (this.player.body.velocity.y == 0)
-        // this.player.body.velocity.y -= 50;
-    // } else if (this.cursors.down.isDown) {
-      // if (this.player.body.velocity.y == 0)
-        // this.player.body.velocity.y += 50;
-    // } else {
-      // this.player.body.velocity.y = 0;
-    // }
-    // if (this.cursors.left.isDown) {
-      // this.player.body.velocity.x -= 50;
-    // } else if (this.cursors.right.isDown) {
-      // this.player.body.velocity.x += 50;
-    // }
+    if (this.cursors.up.isDown) {
+      if (this.player.body.velocity.y == 0)
+        this.player.body.velocity.y -= 50;
+    } else if (this.cursors.down.isDown) {
+      if (this.player.body.velocity.y == 0)
+        this.player.body.velocity.y += 50;
+    } else {
+      this.player.body.velocity.y = 0;
+    }
+    if (this.cursors.left.isDown) {
+      this.player.body.velocity.x -= 50;
+    } else if (this.cursors.right.isDown) {
+      this.player.body.velocity.x += 50;
+    }
   },
 };
